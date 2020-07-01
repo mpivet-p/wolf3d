2438f4901db3c8ccf7d35db98cec9820e660d521
Maxime PIVET-PIERRE <mpivet-p@e1r8p12.42.fr>
2020-07-01 20:00:12 +0200

    need norm and sort_sprites coding

--- srcs/draw_sprites.c
+++ srcs/draw_sprites.c
@@ -6,13 +6,14 @@
 /*   By: mpivet-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 14:36:50 by mpivet-p          #+#    #+#             */
-/*   Updated: 2020/06/30 19:13:08 by mpivet-p         ###   ########.fr       */
+/*   Updated: 2020/07/01 19:59:52 by mpivet-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
+#include <math.h>
 
-static void	get_prite_dist(t_core *wolf, int *sprite_order)
+static void	get_sprite_dist(t_core *wolf, int *sprite_order)
 {
 	t_sprite	*sprt;
 	t_vector	*pos;
@@ -21,15 +22,16 @@ static void	get_prite_dist(t_core *wolf, int *sprite_order)
 
 	i = 0;
 	pos = &(wolf->cam.pos);
-	sprt = &(wolf->world.sprites);
-	while (i < sprite_nbr)
+	sprt = (wolf->world.sprites);
+	while (i < wolf->world.sprt_nbr)
 	{
 		sprite_order[i] = i;
-		sprite_dist[i] = (pos.x - sprt[i]->x) * (pos.x - sprt[i]->x)
-			+ (pos.y - sprt[i]->y) * (pos.y - sprt[i]->y);
+		sprite_dist[i] = (wolf->cam.pos.x - sprt[i].x)
+			* (wolf->cam.pos.x - sprt[i].x)
+			+ (wolf->cam.pos.y - sprt[i].y) * (wolf->cam.pos.y - sprt[i].y);
 		i++;
 	}
-	sort_sprites(sprite_order, sprite_dist, )
+	sort_sprites(sprite_order, sprite_dist, wolf->world.sprt_nbr);
 }
 
 static void	draw_vert_sprite(t_core *wolf, t_vector *sprite_screen
@@ -37,34 +39,33 @@ static void	draw_vert_sprite(t_core *wolf, t_vector *sprite_screen
 			, t_vecint *draw_start, double *z_buffer, int8_t tex_id)
 {
 	t_vecint	tex;
-	int			stripe;
+	int			x;
 	int			color;
 	int			d;
 	int			y;
 
-	stripe = draw_start->x;
-	while (stripe < draw_end->x)
+	x = draw_start->x;
+	while (x < draw_end->x)
 	{
-		tex.x = (256 * (stripe  - (-1 * sprite_dim->x / 2 + sprite_screen->x)) * TEX_WIDTH / sprite_dim->x) / 256;
-		if (transform->y > 0 && stripe > 0 && stripe < SIMG_X && transform->y < z_buffer[stripe])
+		tex.x = (256 * (x  - (-1 * sprite_dim->x / 2 + sprite_screen->x)) * TEX_WIDTH / sprite_dim->x) / 256;
+		if (transform->y > 0 && x > 0 && x < SIMG_X && transform->y < z_buffer[x])
 		{
 			y = draw_start->y;
-        	for (y < draw_end->y)
+        	while (y < draw_end->y)
 			{
-        		d = (y) * 256 - h * 128 + sprite_dim.y * 128;
-        		tex.y = ((d * TEX_HEIGHT) / sprite_dim.y) / 256;
-        		color = wolf->world.sprite[tex_id][TEX_WIDTH * texY + texX];
-				//On filtre la couleur noir (transparence)
+        		d = (y) * 256 - SIMG_Y * 128 + sprite_dim->y * 128;
+        		tex.y = ((d * TEX_HEIGHT) / sprite_dim->y) / 256;
+        		color = wolf->world.sprt_tex[tex_id][TEX_WIDTH * tex.y + tex.x];
         		if ((color & 0x00FFFFFF) != 0)
-					set_pixel(wolf->img, stripe, y, color;
+					set_pixel(wolf->img, x, y, color);
 				y++;
         	}
 		}
-		stripe++;
+		x++;
 	}
 }
 
-static void	machin(t_core *wolf, t_sprite *cur_sprite, double *correc)
+static void	prepare_sprites(t_core *wolf, t_sprite *curr_sprite, double correc)
 {
 	t_vecint	sprite_screen;
 	t_vecint	sprite_dim;
@@ -73,24 +74,19 @@ static void	machin(t_core *wolf, t_sprite *cur_sprite, double *correc)
 	t_vecint	draw_end;
 	t_vector	sprite;
 
-	//Calcul de la position relative
-	sprite.x = cur_sprite.x - wolf->cam.pos.x;
-	sprite.y = cur_sprite.y - wolf->cam.pos.y;
-	//Application de la matrice inverse
+	sprite.x = curr_sprite->x - wolf->cam.pos.x;
+	sprite.y = curr_sprite->y - wolf->cam.pos.y;
 	transform.x = correc * (wolf->cam.dir.y * sprite.x - wolf->cam.dir.x * sprite.y);
 	transform.x = correc * (-1 * wolf->cam.plane.y * sprite.x - wolf->cam.plane.x * sprite.y);
 	sprite_screen.x = (int)(SIMG_X / 2) * (1 + transform.x / transform.y);
-	//Calcul de la hauteur du sprite
-	sprite_dim.y = fabs((int)(SIMG_Y / transform.y));
-	//Calcul du plus haut et du plus bas pixel a colorier
+	sprite_dim.y = (int)fabs(SIMG_Y / transform.y);
 	draw_start.y = (-1 * sprite_dim.y) / 2 + SIMG_Y / 2;
 	if (draw_start.y < 0)
 		draw_start.y = 0;
 	draw_end.y = sprite_dim.y / 2 + SIMG_Y / 2;
 	if (draw_end.y < 0)
 		draw_end.y = SIMG_Y - 1;
-	//Calcul de la largeur du sprite
-	sprite_dim.x = fabs(SIMG_Y / tramsform.y);
+	sprite_dim.x = fabs(SIMG_Y / transform.y);
 	draw_start.x = (-1 * sprite_dim.x) / 2 + sprite_screen.x;
 	if (draw_start.x < 0)
 		draw_start.x = 0;
@@ -112,7 +108,7 @@ void	draw_sprites(t_core *wolf)
 	get_sprite_dist(wolf, sprite_order);
 	while (i < wolf->world.sprt_nbr)
 	{
-		machin(wolf, wolf->world.sprite[i], );
+		prepare_sprites(wolf, &(wolf->world.sprites[i]), correction);
 		i++;
 	}
 }
