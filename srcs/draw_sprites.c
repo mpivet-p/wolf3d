7135ce0ec1e9cf4352821d6da370c7820075cc7a
Maxime PIVET-PIERRE <mpivet-p@student.42.fr>
2020-07-18 14:16:46 +0200

    draw_end.x issue : wrong value

--- srcs/draw_sprites.c
+++ srcs/draw_sprites.c
@@ -6,7 +6,7 @@
 /*   By: mpivet-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 14:36:50 by mpivet-p          #+#    #+#             */
-/*   Updated: 2020/07/18 11:59:35 by mpivet-p         ###   ########.fr       */
+/*   Updated: 2020/07/18 14:15:23 by mpivet-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,15 +69,15 @@ static void	draw_sprite_vert(t_core *wolf, int sprite_screen_x
 	(void)wolf;
 	(void)color;
 	x = draw_start->x;
-	while (x < draw_end->x && x < SIMG_X)
+	while (x < draw_end->x)
 	{
-		tex.x = (256 * (x  - (-1 * sprite_size / 2 + sprite_screen_x)) * TEX_WIDTH / sprite_size) / 256;
+		tex.x = (256 * (x  - (sprite_screen_x - sprite_size / 2)) * TEX_WIDTH / sprite_size) / 256;
 		if (transform->y > 0 && x > 0 && x < SIMG_X && transform->y < z_buffer[x])
 		{
 			y = draw_start->y;
         	while (y < draw_end->y)
 			{
-        		d = (y) * 256 - SIMG_Y * 128 + sprite_size * 128;
+        		d = y * 256 - SIMG_Y * 128 + sprite_size * 128;
         		tex.y = ((d * TEX_HEIGHT) / sprite_size) / 256;
         	//	color = wolf->world.sprt_tex[tex_id][TEX_WIDTH * tex.y + tex.x];
         		color = wolf->world.texture[tex_id][TEX_WIDTH * tex.y + tex.x];
@@ -99,7 +99,6 @@ static void	prepare_sprites(t_core *wolf, t_sprite *curr_sprite, double correc, 
 	int			sprite_screen_x;
 	int			sprite_size;
 
-	(void)z_buffer;
 	sprite.x = curr_sprite->x - wolf->cam.pos.x;
 	sprite.y = curr_sprite->y - wolf->cam.pos.y;
 	transform.x = correc * (wolf->cam.dir.y * sprite.x - wolf->cam.dir.x * sprite.y);
@@ -110,16 +109,17 @@ static void	prepare_sprites(t_core *wolf, t_sprite *curr_sprite, double correc, 
 	if (draw_start.y < 0)
 		draw_start.y = 0;
 	draw_end.y = sprite_size / 2 + SIMG_Y / 2;
-	if (draw_end.y < 0)
+	if (draw_end.y < 0 || draw_end.y > SIMG_Y)
 		draw_end.y = SIMG_Y - 1;
 	sprite_size = (int)fabs(SIMG_Y / transform.y);
 	draw_start.x = sprite_screen_x - sprite_size / 2;
 	if (draw_start.x < 0)
 		draw_start.x = 0;
 	draw_end.x = sprite_size / 2 + sprite_screen_x;
-	if (draw_end.x < 0)
+//	draw_end.x = (SIMG_X / transform.x) / 2 + sprite_screen_x;
+	if (draw_end.x < 0 || draw_end.x > SIMG_X)
 		draw_end.x = SIMG_X - 1;
-	printf("%d %d\n", draw_start.x, draw_end.x);
+	printf("%d %d %d %d\n", draw_start.x, draw_end.x, draw_start.y, draw_end.y);
 	draw_sprite_vert(wolf, sprite_screen_x, sprite_size, &transform, &draw_end, &draw_start, z_buffer, curr_sprite->tex_id);
 }
 
@@ -131,11 +131,12 @@ void	draw_sprites(t_core *wolf, double *z_buffer)
 
 	i = 0;
 	correction = 1.0 / (wolf->cam.plane.x * wolf->cam.dir.y - wolf->cam.dir.x * wolf->cam.plane.y);
-	
+
 	wolf->world.sprt_nbr = 1;
 	wolf->world.sprites[0].x = 5;
 	wolf->world.sprites[0].y = 10;
 	wolf->world.sprites[0].tex_id = 8;
+
 	get_sprite_dist(wolf, sprite_order);
 	while (i < wolf->world.sprt_nbr)
 	{
