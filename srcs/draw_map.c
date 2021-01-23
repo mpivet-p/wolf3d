941b2b97ae111d4f48c65cc9f3167fa7625548a2
Maxime PIVET-PIERRE <mpivet-p@student.42.fr>
2021-01-23 17:07:04 +0100

    Trying to fix map view rays + need to invert y axis

--- srcs/draw_map.c
+++ srcs/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: mpivet-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 15:42:11 by mpivet-p          #+#    #+#             */
-/*   Updated: 2020/08/16 16:04:25 by mpivet-p         ###   ########.fr       */
+/*   Updated: 2021/01/23 17:06:02 by mpivet-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,24 @@
 
 void	draw_map(t_core *wolf, t_ray *ray, int *step)
 {
+	t_vector delta_dist;
+
+	delta_dist = get_delta_dist(&(ray->dir));
 	if (wolf->cam.mode != WLF_MAP)
 		return ;
 	draw_square(wolf, ray->map[0], ray->map[1], 0x0000CC);
 	if (ray->side == 0)
 	{
-		draw_ray(wolf, set_vec(ray->map[0] + (step[0] == -1)
-			, wolf->cam.pos.y + (ray->dir.y * (ray->side_dist.x - 1))), 0xFF0000);
-		if (wolf->cam.pos.y + (ray->dir.y * (ray->side_dist.x - 1)) < 0)
-		{
-			ft_putstr_fd("Error :  ", STDERR_FILENO);
-			dprintf(STDOUT_FILENO, "%f %f %f %f\n", ray->dir.x, ray->dir.y, ray->side_dist.x, ray->side_dist.y);
-		}
+		if (step[0] < 0)
+			draw_ray(wolf, set_vec(ray->map[0] + 1, ray->map[1] + ), 0xFF0000);
+		else
+			draw_ray(wolf, set_vec(ray->map[0], ray->map[1] - delta_dist.x), 0xFF0000);
 	}
 	else
 	{
-		draw_ray(wolf, set_vec(
-			wolf->cam.pos.x + (ray->dir.x * (
-			ray->side_dist.y - 1)), ray->map[1] + (step[1] == -1)), 0x00FF00);
+		if (step[1] < 0)
+			draw_ray(wolf, set_vec(ray->map[0], ray->map[1] + 1), 0x00FF00);
+		else
+			draw_ray(wolf, set_vec(ray->map[0], ray->map[1]), 0x00FF00);
 	}
 }
