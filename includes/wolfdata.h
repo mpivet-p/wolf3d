0c8d98bc5a383db853b765d68fbfa306e181d0cc
madvid <mdavid@student.42.fr>
2021-05-11 13:47:07 +0200

    add of define for linux

--- includes/wolfdata.h
+++ includes/wolfdata.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wolfdata.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpivet-p <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mdavid <mdavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/04 01:19:54 by mpivet-p          #+#    #+#             */
-/*   Updated: 2020/08/14 13:33:57 by mpivet-p         ###   ########.fr       */
+/*   Updated: 2021/05/11 13:34:45 by mdavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,43 @@
 # define COL_BLUE	0x0589F5
 # define COL_YELLO	0xF5A511
 # define COL_PURPLE	0xC017E6
-# define KEY_UP		126
-# define KEY_DOWN	125
-# define KEY_RIGHT	124
-# define KEY_LEFT	123
-# define KEY_W		13
-# define KEY_S		1
-# define KEY_A		0
-# define KEY_D		2
-# define KEY_Q		12
-# define KEY_E		14
-# define KEY_ESC	53
-# define KEY_TAB	48
-# define KEY_SPA	49
-# define KEY_PGUP	116
-# define KEY_PGDWN	121
-# define KEY_LESS	69
-# define KEY_PLUS	78
+//
+# ifdef __linux__
+#  define KEY_UP	65362
+#  define KEY_DOWN	65364
+#  define KEY_LEFT	65361
+#  define KEY_RIGHT	65363
+#  define KEY_W		119
+#  define KEY_A		97
+#  define KEY_S		115
+#  define KEY_D		100
+#  define KEY_Q		113
+#  define KEY_E		101
+#  define KEY_ESC	65307
+#  define KEY_PGUP	65365
+#  define KEY_PGDWN	65366
+#  define KEY_LESS	45
+#  define KEY_PLUS	61
+# else
+#  define KEY_UP	126
+#  define KEY_DOWN	125
+#  define KEY_RIGHT	124
+#  define KEY_LEFT	123
+#  define KEY_W		13
+#  define KEY_S		1
+#  define KEY_A		0
+#  define KEY_D		2
+#  define KEY_Q		12
+#  define KEY_E		14
+#  define KEY_ESC	53
+#  define KEY_TAB	48
+#  define KEY_SPA	49
+#  define KEY_PGUP	116
+#  define KEY_PGDWN	121
+#  define KEY_LESS	69
+#  define KEY_PLUS	78
+# endif
+//
 
 # define MAPWIDTH	24
 # define MAPHEIGHT	24
